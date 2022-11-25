5b3e0ebefdd662ab3789a15cfb8c5515aeed40b7
Jodie Drover <jodiedrover@live.com>
2022-11-25 11:01:26 -0500

    Set up a folder and files for heler functions

---
+++ src/helpers/playerHelpers.js
@@ -0,0 +1,12 @@
+// playerHelpers.js
+
+// Declarations starting with export will be added to module.exports object
+// export default... at the bottom will not work for multiple exports
+
+export const preparePlayerData = () => {
+
+};
+
+export const addWinsToPlayer = () => {
+
+};
