259abee6703780751907c8a6ba1090cc3bed8842
Jodie Drover <jodiedrover@live.com>
2022-11-25 10:09:21 -0500

    Create Player and PlayerList components

---
+++ src/components/PlayerList.js
@@ -0,0 +1,12 @@
+// PlayerList.js
+import React from "react"; // optional
+import Player from "./Player";
+
+function PlayerList() {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      <Player />
+    </section>
+  );
+}
