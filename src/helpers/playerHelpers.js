7d24ae898e7e7befb09fa76a732d094cb6eb221e
Jodie Drover <jodiedrover@live.com>
2022-11-25 11:08:29 -0500

    Populate helper functions

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -3,10 +3,27 @@
 // Declarations starting with export will be added to module.exports object
 // export default... at the bottom will not work for multiple exports
 
-export const preparePlayerData = () => {
-
+export const preparePlayerData = (playerData) => {
+  return Object.values(playerData);
 };
 
-export const addWinsToPlayer = () => {
+export const addWinsToPlayer = (playerDataArray, matchData) => {
+  return playerDataArray.map(player => {
+    // Calculating the # of wins in the matchData
+    const currentWins = matchData.reduce((accumulator, match) => {
+      // Adds a win if the gamerTag matches
+      if (match.winner === player.gamerTag) {
+        return accumulator += 1;
+      } else {
+        return accumulator;
+      }
+    }, 0);
 
-};
+    // Assigns the value to the wins key
+    player.wins = currentWins;
+
+    return player
+  });
+}
+
+// output: [{player}, {player}] (each player having a win key and a numerical value)
