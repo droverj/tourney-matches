119c15ddb98b3ce394cee02f83f0847880ca7096
Jodie Drover <jodiedrover@live.com>
2022-11-27 14:29:59 -0500

    Import data and helper functions into parent component - App.js

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,10 +1,9 @@
 // MatchList.js
-import matchData from '../data/matchData';
 import Match from './Match';
 
 function MatchList(props) {
 
-  const oneMatch = matchData[0];
+  const oneMatch = props.matchData[0];
 
   return (
     <section className="PlayerList MatchList">
